cd8aa1ee24d9652284e668290123f20b04d48887
Ahmet MALAL <ahmetmalal2014@outlook.com>
2018-01-17 15:52:50 +0300

    first commit

---
+++ Art Book/detailedVC.swift
@@ -0,0 +1,35 @@
+//
+//  detailedVC.swift
+//  Art Book
+//
+//  Created by Ahmet Malal on 16.01.2018.
+//  Copyright © 2018 Ahmet Malal. All rights reserved.
+//
+
+import UIKit
+
+class detailedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
